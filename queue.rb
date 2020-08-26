a4cdcbe2bc7b1fde2ac0440d4d8210d78b1b574b
Elbie-em <emoonga1993@gmail.com>
2020-08-26 19:11:46 +0200

    Add queue module with queue class

--- queue.rb
+++ queue.rb
@@ -1,1 +1,22 @@
 require_relative 'linkedlistb.rb'
+
+include LS
+
+module QueueModule
+
+  class Queue
+
+    def initialize
+			@list = LinkedList.new
+		end
+		
+		def add(number)
+			@list.add(number)
+		end
+		
+		def remove
+			@list.remove(0)
+		end
+	end
+	
+end
